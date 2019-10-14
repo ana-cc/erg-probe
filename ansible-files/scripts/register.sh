3ed81d018593611ce87ad3d621c349d87df8d0f8
Ana Custura <ana@netstat.org.uk>
2019-10-14 21:40:22 +0100

    Better script to give ip and onion address

--- ansible-files/scripts/register.sh
+++ ansible-files/scripts/register.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 OSERV=`cat /var/lib/tor/ssh/hostname`
+ADDR=`ip route get 8.8.4.4 | head -1 | awk '{print $7}'`
 
 cat <<EOF
 {
     "onion_service" : "$OSERV"
+    "ip_addr" : "$ADDR"
 }
 EOF
 
-cat /tmp/facts_cache/127.0.0.1 
